d81c9f0e2d04e7c02817cb16f014e07d4984648b
Toby Merlet <tmerlet@gmail.com>
2016-12-13 19:33:29 +0700

    Add read function

--- api/src/InMemoryDb.js
+++ api/src/InMemoryDb.js
@@ -1,14 +1,23 @@
 let uid;
+let db;
 
 const create = ({ title }) => {
   uid++;
-  return { id: uid, title }
+  const newFilm = { id: uid, title };
+  db[uid] = newFilm;
+  return newFilm;
+};
+
+const read = () => {
+  return Object.keys(db).map(key => db[key]);
 };
 
 export default (seed = 0) => {
+  db = {};
   uid = seed;
   return {
     uid,
-    create
+    create,
+    read
   };
 };
--- api/src/InMemoryDb.spec.js
+++ api/src/InMemoryDb.spec.js
@@ -20,4 +20,27 @@ describe('InMemoryDb', () => {
       expect(actual).to.deep.equal({ id: 2, title: 'World'});
     });
   });
+
+  describe('.read', () => {
+    let db;
+    beforeEach(() => {
+      db = InMemoryDb();
+    })
+
+    it('returns empty object when no films exist', () => {
+      expect(db.read()).to.deep.equal([]);
+    });
+
+    it('returns film when only one film exists', () => {
+      db.create({ title: 'Hello' });
+      expect(db.read()).to.deep.equal([{ id: 1, title: 'Hello'}]);
+    });
+
+    it('returns all films when multiple films exist', () => {
+      db.create({ title: 'Hello' });
+      db.create({ title: ',' });
+      db.create({ title: 'World' });
+      expect(db.read()).to.deep.equal([{ id: 1, title: 'Hello'}, { id: 2, title: ','}, { id: 3, title: 'World'}]);
+    });
+  });
 });
