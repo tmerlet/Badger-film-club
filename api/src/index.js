ba6d2a2392e2ec4cf448740e244215bd4f7ad17b
Toby Merlet <tmerlet@gmail.com>
2016-12-17 12:28:13 +0700

    Pass port in from env

--- api/src/index.js
+++ api/src/index.js
@@ -1,4 +1,7 @@
 import app from './server';
 import InMemoryDb from './persistence/InMemoryDb';
 
-app(InMemoryDb());
+const port = process.env.PORT || 8080;
+const db = InMemoryDb();
+
+app(db, port);
