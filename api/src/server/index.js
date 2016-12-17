73333994ad681d1a329ca84a879c7f3513844eda
Toby Merlet <tmerlet@gmail.com>
2016-12-17 10:41:20 +0700

    Tests for api

--- api/src/server/index.js
+++ api/src/server/index.js
@@ -1,25 +1,23 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
-export default db => {
+export default (db, port) => {
 
   const app = express();
 
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
 
-  app.get('/films', (req, res) => {
-    res.status(200).send(db.read());
-  });
+  app.get('/films', (req, res) => res.status(200).send(db.read()));
 
   app.get('/films/:id', (req, res) => {
     const { id } = req.params;
     const film = db.readById(id);
     if (film === undefined) {
-      res.status(404).send(`${id} not found`);
+      return res.status(404).send({ message: `${id} not found` });
 
     } else {
-      res.status(200).send(film);
+      return res.status(200).send(film);
     }
   });
 
@@ -27,11 +25,11 @@ export default db => {
     const { title } = req.body;
 
     if (title === undefined) {
-      res.status(400).send(`No title supplied in body to create`);
+      return res.status(400).send({ message: `No title supplied` });
     }
 
     const newFilm = db.create(title);
-    res.status(200).send(`http://localhost/films/${newFilm.id}`);
+    return res.status(201).location(`films/${newFilm.id}`).send(newFilm);
   });
 
   app.put('/films/:id', (req, res) => {
@@ -39,20 +37,20 @@ export default db => {
     const { title } = req.body;
 
     if (title === undefined) {
-      res.status(400).send(`No title supplied to update film: ${id}`);
+      return res.status(400).send({ message: `No title supplied to update film: ${id}` });
     }
 
     const updatedFilm = db.update(id, title);
 
     if (updatedFilm === undefined) {
-      res.status(404).send(`${id} not found`);
+      return res.status(404).send({ message: `${id} not found` });
 
     } else {
-      res.status(200).send(`http://localhost/films/${updatedFilm.id}`);
+      return res.status(200).location(`films/${updatedFilm.id}`).send(updatedFilm);
     }
   });
 
-  app.listen(8080, () => {
-    console.log('Server listening on port 8080');
+  return app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
   });
 }
---
+++ api/src/server/index.spec.js
@@ -0,0 +1,163 @@
+import request from 'supertest';
+
+import app from './';
+import InMemoryDb from '../persistence/InMemoryDb';
+
+describe('films', () => {
+
+  describe('GET /films', () => {
+    let db;
+    let server;
+
+    beforeEach(() => {
+      db = InMemoryDb();
+    });
+
+    afterEach(() => {
+      server.close()
+    });
+
+    it('responds with content type json', (done) => {
+      server = app(db, 8080);
+      request(server)
+        .get('/films')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(done);
+    });
+
+    it('responds with status 200 and empty array when no films exist', (done) => {
+      server = app(db, 8080);
+      request(server)
+        .get('/films')
+        .expect(200, [])
+        .end(done);
+    });
+
+    it('responds with status 200 and array of existing films', (done) => {
+      db.create('The Wizard of Oz (1939)');
+      db.create('All About Eve (1950)');
+      db.create('Inside Out (2015)');
+      server = app(db, 8080);
+      request(server)
+        .get('/films')
+        .expect(200, [
+          { id: 1, title: 'The Wizard of Oz (1939)' },
+          { id: 2, title: 'All About Eve (1950)' },
+          { id: 3, title: 'Inside Out (2015)' }
+        ])
+        .end(done);
+    });
+  });
+
+  describe('GET /films/:id', () => {
+    let server;
+    let req;
+
+    before(() => {
+      const db = InMemoryDb();
+      db.create('The Wizard of Oz (1939)');
+      db.create('All About Eve (1950)');
+      db.create('Inside Out (2015)');
+      server = app(db, 8080);
+      req = request(server);
+    });
+
+    after(() => server.close());
+
+    it('responds with status code 400 when title is not supplied', (done) => {
+      req
+        .get('/films/100')
+        .expect('Content-Type', /json/)
+        .expect(404, { message: '100 not found' })
+        .end(done);
+    });
+
+    it('responds with status code 200 and film', (done) => {
+      req
+        .get('/films/1')
+        .expect('Content-Type', /json/)
+        .expect(200, { id: 1, title: 'The Wizard of Oz (1939)' })
+        .end(done);
+    });
+  });
+
+  describe('PUT /films/:id', () => {
+    let server;
+    let req;
+
+    before(() => {
+      const db = InMemoryDb();
+      db.create('The Wizard of Oz (1939)');
+      db.create('All About Eve (1950)');
+      db.create('Inside Out (2015)');
+      server = app(db, 8080);
+      req = request(server);
+    });
+
+    after(() => server.close());
+
+    it('responds with status code 200 and updated film', (done) => {
+      req
+        .put('/films/1')
+        .send({ title: 'Hello, world!' })
+        .expect('Content-Type', /json/)
+        .expect('Location', 'films/1')
+        .expect(200, { id: 1, title: 'Hello, world!' })
+        .end(done);
+    });
+
+    it('responds with status code 404 when film id does not exist', (done) => {
+      req
+        .put('/films/100')
+        .send({ title: 'Hello, world!' })
+        .expect('Content-Type', /json/)
+        .expect(404, { message: '100 not found' })
+        .end(done);
+    });
+
+    it('responds with status code 400 when film request body does not include title', (done) => {
+      req
+        .put('/films/1')
+        .send({ t: 'Hello, world!' })
+        .expect('Content-Type', /json/)
+        .expect(400, { message: `No title supplied to update film: 1` })
+        .end(done);
+    });
+  });
+
+  describe('POST /films', () => {
+    let server;
+    let req;
+
+    before(() => {
+      const db = InMemoryDb();
+      db.create('The Wizard of Oz (1939)');
+      db.create('All About Eve (1950)');
+      db.create('Inside Out (2015)');
+      server = app(db, 8080);
+      req = request(server);
+    });
+
+    after(() => server.close());
+
+    it('responds with status code 201 and newly created film', (done) => {
+      req
+        .post('/films')
+        .send({ title: 'Hello, world!' })
+        .expect('Location', 'films/4')
+        .expect('Content-Type', /json/)
+        .expect(201, { id: 4, title: 'Hello, world!' })
+        .end(done);
+    });
+
+    it('responds with status code 400 when title is not supplied', (done) => {
+      req
+        .post('/films')
+        .send({})
+        .expect('Content-Type', /json/)
+        .expect(400, { message: 'No title supplied' })
+        .end(done);
+    });
+  });
+});
