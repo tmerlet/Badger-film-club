4cdb97b3d112e18ea16e01b55aa8057446ac8eb5
Toby Merlet <tmerlet@gmail.com>
2017-02-27 20:05:35 +0000

    Add films to be rendered

--- films_ui/src/components/Films/index.js
+++ films_ui/src/components/Films/index.js
@@ -6,11 +6,32 @@ const Films = (props) => {
       {props.loading &&
         'Loading!'
       }
+
+      {!props.loading &&
+        <div>
+          {props.films.length > 0 &&
+            <ul>
+              {props.films.map((film) => {
+                return <li key={`film-${film.id}`}>{film.title}</li>;
+              })}
+            </ul>
+          }
+          {props.films.length === 0 &&
+            <div>No films!</div>
+          }
+        </div>
+      }
     </div>
   );
 };
 
 Films.propTypes = {
+  films: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      title: PropTypes.string
+    })
+  ),
   loading: PropTypes.bool,
 };
 
--- films_ui/src/components/Films/index.spec.js
+++ films_ui/src/components/Films/index.spec.js
@@ -4,17 +4,57 @@ import { expect } from 'chai';
 
 import Films from './';
 
-describe.only('Films', () => {
+describe('Films', () => {
 
-  it('renders loading message when loading is true', () => {
-    const component = shallow(<Films loading />);
-    expect(component.html()).to.contain('Loading!');
+  describe('loading message', () => {
+
+    it('renders when loading is true', () => {
+      const component = shallow(<Films loading />);
+      expect(component.html()).to.contain('Loading!');
+    });
+
+    it('does not render when loading is false', () => {
+      const component = shallow(<Films films={[]} loading={false} />);
+      expect(component.html()).to.not.contain('Loading!');
+    });
+
   });
 
-  it('does not render loading message when loading is false', () => {
-    const component = shallow(<Films loading={false} />);
-    expect(component.html()).to.not.contain('Loading!');
+  it('renders given film when loading is false', () => {
+    const component = shallow(
+      <Films
+        films={[{ id: 0, title: 'chicken' }]}
+        loading={false}
+      />
+    );
+
+    expect(component.find('li').length).to.equal(1);
+    expect(component.find('li').at(0).props().children).to.equal('chicken');
   });
 
+  it('renders given films when loading is false', () => {
+    const component = shallow(
+      <Films
+        films={[{ id: 0, title: 'chicken' }, { id: 1, title: 'shard' }]}
+        loading={false}
+      />
+    );
+
+    expect(component.find('li').length).to.equal(2);
+    expect(component.find('li').at(0).props().children).to.equal('chicken');
+    expect(component.find('li').at(1).props().children).to.equal('shard');
+  });
+
+  it('renders a no films message when loading is false and films is empty', () => {
+    const component = shallow(
+      <Films
+        films={[]}
+        loading={false}
+      />
+    );
+
+    expect(component.find('li').length).to.equal(0);
+    expect(component.html()).to.contain('No films!');
+  });
 
 });
